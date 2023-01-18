9b73e4c50cffab14b0f366b3b3411c01aea152fc
Iorga Florin <iorgaflorinalexandru@gmail.com>
2023-01-18 23:53:51 +0200

    Subreddit Description UI

--- RedditClone/src/app/subreddits/components/subreddit-description/subreddit-description.component.ts
+++ RedditClone/src/app/subreddits/components/subreddit-description/subreddit-description.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'subreddit-description',
   templateUrl: './subreddit-description.component.html',
   styleUrls: ['./subreddit-description.component.css']
 })
-export class SubredditDescriptionComponent {
+export class SubredditDescriptionComponent{
 
+  @ViewChild("editInput",{static: false}) editInput! : ElementRef;
+  @Input() description : string  = "";
+
+  editDescription: boolean = false;
+
+  changeTemplate(){
+    this.editDescription = !this.editDescription;
+    setTimeout(() => this.editInput.nativeElement.focus(), 0);  }
 }
