dca1ff72a44b970ed7e06a9d04a3c77c05053594
Iorga Florin <iorgaflorinalexandru@gmail.com>
2024-02-21 00:03:27 +0200

    Refactoring Tailwind, Adding a light theme

--- src/RedditClone.UI/redditClone-ui/tailwind.config.js
+++ src/RedditClone.UI/redditClone-ui/tailwind.config.js
@@ -1,10 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          text: {
+            dark: '#D7DADC',
+            light: '#1c1c1c'
+          },
+          bg: {
+            dark: '#1A1A1B',
+            light: '#ffffff'
+          },
+          highlight: {
+            dark: '#303030',
+            light: '#e7e7e7',
+          }
+        },
+        secondary: {
+          dark: '#343536',
+          light: '#edeff1',
+          text: {
+            dark: '#818384',
+            light: 'TOUSE'
+          }
+        },
+        tertiary: {
+          dark: 'TOUSE',
+          light: 'TOUSE',
+          bg: {
+            dark: '#272729',
+            light: '#f6f7f8'
+          }
+        },
+        accent: {
+          orange: '#d93a00',
+          blue: '#0079d3'
+        }
+      }
+    },
   },
   plugins: [],
 }
