774bb39f93952519c90774291bbed4a59b4ae209
Iorga Florin <iorgaflorinalexandru@gmail.com>
2024-02-24 19:29:38 +0200

    Scrollbar white theme

--- src/RedditClone.UI/redditClone-ui/src/app/posts/services/post.service.ts
+++ src/RedditClone.UI/redditClone-ui/src/app/posts/services/post.service.ts
@@ -17,7 +17,7 @@ export class PostService {
     if(community) params = params.append("community",community);
 
     return this.http.get<Post[]>('/api/post',{ params }).pipe(
-      delay(200000)
+      delay(2000)
     );
   }
   
